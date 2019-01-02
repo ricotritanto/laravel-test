3af64582ae759868a2fdaf19df2db7c3780b37c2
rico <rico.tritanto89@gmail.com>
2019-01-02 22:44:29 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,8 @@ Route::group(['prefix' => 'kategori'], function()
 {
     Route::get('/', 'KategoryController@index');
     Route::get('/new', 'KategoryController@create');
-    Route::post('/', 'KategoryController@save');
+    Route::post('/', 'KategoryController@save');    
+    Route::get('/{id}', 'KategoryController@edit');
+    Route::put('/{id}', 'KategoryController@update');
+    Route::delete('/{id}', 'KategoryController@delete');
 });
