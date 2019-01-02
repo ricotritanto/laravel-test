3c7341fb2c3ba2539468be820394a4452c0ec4d9
rico <rico.tritanto89@gmail.com>
2019-01-02 22:41:00 +0700

    Update KategoryController.php

--- app/Http/Controllers/KategoryController.php
+++ app/Http/Controllers/KategoryController.php
@@ -9,29 +9,57 @@ use App\Repository\KategoriRepository;
 class KategoryController extends Controller
 {
 
-     public function index()
+     Public Function Index()
     {
     	$kategoryRepo=new KategoriRepository;
        	$kategoris = $kategoryRepo->getCategoryList();
     	return view('kategori.index', compact('kategoris')); // 3
     }
    	
-   	 public function create()
+   	 Public Function Create()
     {
         return view('kategori.create');
     }
 
-    public function save(Request $request)
+    Public Function Save(Request $request)
     {
-        $this->validate($request, [
-            'name' => 'required|string|max:100',
-        ]);
+        // $this->validate($request, [
+        //     'name' => 'required|string|max:100'
+        // ]);
+        $aa = $request->all();
+        $name = $aa['name'];
+        // $name= ['name'=>$request->name];
         $kategoryRepo=new KategoriRepository;
-	}
-    public function createCategory($name)
-    {
-       	$kategoris = $kategoryRepo->createCategory($name);
-        if($kategoris) return redirect('/kategori')->with(['success' => '<strong>' . $kategori->name . '</strong> Telah disimpan']);
+	   // function createCategory($name)
+    //    	{
+        $kategoris = $kategoryRepo->createCategory($name);
+        if($kategoris) return redirect('/kategori')->with(['success' => '<strong>' . $kategoris->name . '</strong> Telah disimpan']);
         else return redirect('/kategori/new')->with(['error' => $e->getMessage()]); 
+        // }
+       
+    }
+
+    //fungsi update kategori
+    Public Function Edit($id)
+    {
+    	$kategoryRepo=new KategoriRepository;
+    	$kategoris = $kategoryRepo->getCategoryId($id);
+    	return view('kategori.edit', compact('kategoris'));
+    }
+
+    Public Function Update($id)
+    {
+
+    }
+
+    Public Function Delete($id)
+    {
+    	$kategoryRepo=new KategoriRepository;
+    	$kategoris = $kategoryRepo->DeleteCategory($id);
+        
+        print_r($kategoris);exit();
+    	if($kategoris) return redirect('/kategori')->with(['success' => '<strong>' . $kategoris->name . '</strong> Telah disimpan']);
+        else return redirect('/kategori/new')->with(['error' => $e->getMessage()]); 
+        // ->with(['success' => '</strong>' . $kategori->name . '</strong> Dihapus']); //
     }
 }
