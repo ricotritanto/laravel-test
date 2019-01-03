f54d7012695772124b6f79c2a40e9ed0d0084d69
rico <rico.tritanto89@gmail.com>
2019-01-03 21:46:45 +0700

    Add files via upload

--- app/Repository/KategoriRepository.php
+++ app/Repository/KategoriRepository.php
@@ -7,7 +7,11 @@ use App\Model\transaction;
 class KategoriRepository{
 
     Public Function GetCategoryList(){
+<<<<<<< HEAD
+        return kategori::orderBy('name', 'ASC')->get(); // 2 
+=======
         return kategori::orderBy('created_at', 'DESC')->get(); // 2 
+>>>>>>> 64e885030f4366604f26dedba683a190b10c6d40
     }
 
     Public Function GetTransactionList(){
@@ -15,11 +19,22 @@ class KategoriRepository{
     }
 
     Public Function CreateCategory($name){
+<<<<<<< HEAD
+         return kategori::create(['name'=>$name]);
+    }
+
+    Public Function UpdateCategory($id, $name)
+    {
+        return kategori::Where('id', $id)->update(['name'=>$name]);
+    } 
+
+=======
          return kategori::create(['name'=>$name]);  
        
         
     }
 
+>>>>>>> 64e885030f4366604f26dedba683a190b10c6d40
     Public Function GetCategoryId($id)
     {
         return kategori::find($id);
