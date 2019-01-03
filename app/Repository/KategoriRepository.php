24c3d44071b6235b1a47cdd419e03cd46e49e501
ricotritanto <rico.tritanto89@gmail.com>
2019-01-03 22:43:36 +0700

    wes jam 11

---
+++ app/Repository/KategoriRepository.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Repository;
+
+use App\Model\kategori;
+use App\Model\transaction;
+
+class KategoriRepository{
+
+    Public Function GetCategoryList(){
+        return kategori::orderBy('name', 'ASC')->get(); // 2 
+        return kategori::orderBy('created_at', 'DESC')->get(); // 2 
+
+    }
+
+    Public Function GetTransactionList(){
+        return kategori::orderBy('created_at', 'DESC')->with('detail')->get(); // 2 
+    }
+
+    Public Function CreateCategory($name){
+         return kategori::create(['name'=>$name]);
+    }
+
+    Public Function UpdateCategory($id, $name)
+    {
+        return kategori::Where('id', $id)->update(['name'=>$name]);
+    } 
+        return kategori::create(['name'=>$name]);  
+       
+        
+    }
+    Public Function GetCategoryId($id)
+    {
+        return kategori::find($id);
+    }
+    Public Function DeleteCategory($id)
+    {
+        return kategori::find($id)->delete(); //QUERY KEDATABASE UNTUK MENGAMBIL DATA BERDASARKAN ID
+        // return kategori::delete();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+//router untuk menu category
+Route::group(['prefix' => 'kategori'], function() 
+{
+    Route::get('/', 'KategoryController@index');
+    Route::get('/new', 'KategoryController@create');
+    // Route::get('/create', 'KategoryController@createCategory');
+    Route::post('/', 'KategoryController@save');
+    Route::get('/{id}', 'KategoryController@edit');
+    Route::put('/{id}', 'KategoryController@update');
+    Route::delete('/{id}', 'KategoryController@delete');
+});
+
+Route::group(['prefix' => 'produks'], function()
+{
+	Route::get('/', 'ProdukController@index');
+    Route::get('/new', 'ProdukController@create');
+    Route::post('/', 'KategoryController@save');    
+    Route::get('/{id}', 'KategoryController@edit');
+    Route::put('/{id}', 'KategoryController@update');
+    Route::delete('/{id}', 'KategoryController@delete');
+});
