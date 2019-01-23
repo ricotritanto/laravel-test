b41f3a51b625c086393c4cdc9537b1009e08b3b3
ricotritanto <rico.tritanto89@gmail.com>
2019-01-23 14:43:26 +0700

    update index

--- resources/views/transaction/index.blade.php
+++ resources/views/transaction/index.blade.php
@@ -2,10 +2,6 @@
 
 @section('content')
 <section class="content-header">
-  <h1>
-    TRANSACTION
-    <!-- <small>advanced tables</small> -->
-  </h1>
   <!-- <ol class="breadcrumb">
     <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
     <li><a href="#">Tables</a></li>
