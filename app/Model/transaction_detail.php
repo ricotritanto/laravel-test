a3567cd13ae097320e4703ad66748573ffd9fd73
ricotritanto <rico.tritanto89@gmail.com>
2019-01-24 15:24:53 +0700

    update status transaksi

--- app/Model/transaction_detail.php
+++ app/Model/transaction_detail.php
@@ -9,17 +9,17 @@ class Transaction_detail extends Model
     protected $fillable = ['id','id_produk','transaction_id', 'qty'];
     protected $table = 'transaction_details';
 
-    public function transaction_detail()
-    {
-    	return $this->hasOne(produks::class, 'id','id_produk');
-    }
     public function produks()
     {
     	return $this->hasOne(produks::class, 'id','id_produk');
     }
-     public function kategoris()
+    //  public function kategoris()
+    // {
+    // 	return $this->hasOne(Kategori::class, 'id','id_kategori');
+    // }
+    public function transaction()
     {
-    	return $this->hasOne(Kategori::class, 'id','id_kategori');
+        return $this->hasMany(transaction::class, 'id','transaction_id');
     }
 
    
